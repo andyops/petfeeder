d09cb82c78ccfac3310ef1a572ba09ab73106e9a
andyops <anasshahmurshad2021.comp@mmcoe.edu.in>
2023-04-04 15:03:50 +0530

    gitignore and application.properties updated

--- src/main/java/com/example/petfeeder/entity/Product.java
+++ src/main/java/com/example/petfeeder/entity/Product.java
@@ -1,9 +1,30 @@
 package com.example.petfeeder.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "client")
 public class Product {
-    private int id ;
+    @Id
+    @GeneratedValue
+    private UUID id ;
     private String name ;
+    private int breakfast;
+    private int lunch;
+    private int dinner;
+
 
 }
 
